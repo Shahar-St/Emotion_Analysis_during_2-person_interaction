fbe08c2f0c5567e2d9a737c55cdfd85fb88cf2d7
Shahar Stahi <shaharstahi@gmail.com>
2022-03-18 14:09:40 +0200

    Added FeaturesReducer skeleton

--- features_extractor/FeaturesReducer.py
+++ features_extractor/FeaturesReducer.py
@@ -1,5 +1,34 @@
 class FeaturesReducer:
 
     @staticmethod
-    def process_model_features(raw_features):
-        pass
+    def process_model_features(raw_features: dict) -> dict:
+        """
+        Gets a dict of all raw features and reduce them to the actual model features
+        :param raw_features: dict of the raw features. Example:
+            raw_features = {
+                'raw_feature1': 4,
+                'raw_feature2': 4.5,
+                'raw_feature3': 12.7,
+            }
+        :return: model_features
+        """
+
+        features_mapper = {
+            'modelFeature1': (('raw_feature1', 'raw_feature2', 'raw_feature3'), FeaturesReducer.func1),
+            'modelFeature2': (('raw_feature1', 'raw_feature2'), min),
+        }
+
+        model_features = {}
+        for feature_name, feature_args in features_mapper.items():
+            args = []
+            for raw_feature in feature_args[0]:
+                args.append(raw_features[raw_feature])
+            model_features[feature_name] = feature_args[1](args)
+
+        return model_features
+
+    @staticmethod
+    def func1(args):
+        a, b, c = args
+        # logic of the feature
+        return max(a, b, c)
--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from model.Model import Model
 
 def main():
     # Features
-    excel_files_dir = ''
+    excel_files_dir = ''  # Todo csv or excel?
     excel_files_path = os.path.join(os.getcwd(), excel_files_dir)
     raw_features = RawFeaturesExtractor.extract_raw_features(excel_files_path)
     model_Features = FeaturesReducer.process_model_features(raw_features)
